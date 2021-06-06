f0b1a58e395ecdc9cd223553937e5bab7589f7e9
chihabSD <chihabahmed2012@hotmail.com>
2021-06-06 15:42:59 +0100

    sss

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
-const amqp = require("amqplib");
-const bp = require("body-parser");
-var channel, connection;
-
-app.use(bp.json());
-connect();
-async function connect() {
-  try {
-    const amqpServer = process.env.RABBITMQ_URL;
-    connection = await amqp.connect(amqpServer);
-    channel = await connection.createChannel();
-    await channel.assertQueue("session");
-  } catch (ex) {
-    console.error(ex);
-  }
-}
-
-const createSession = async (user) => {
-  await channel.sendToQueue("session", Buffer.from(JSON.stringify(user)));
-  await channel.close();
-  await connection.close();
-};
-
-app.post("/", (req, res) => {
-  res.send({ hi: "sss" });
-});
+const amqp = require("amqplib/callback_api");
 
 app.post("/login", (req, res) => {
   const { user } = req.body;
@@ -34,47 +8,47 @@ app.post("/login", (req, res) => {
   res.send(user);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server at port ${PORT}`);
+amqp.connect(process.env.RABBITMQ_URL, (connError, connection) => {
+  if (connError) {
+    throw connError;
+  }
+  // Step 2: Create Channel
+  connection.createChannel((channelError, channel) => {
+    if (channelError) {
+      throw channelError;
+    }
+    // Step 3: Assert Queue
+    const QUEUE = "Justtest";
+    channel.assertQueue(QUEUE);
+    // Step 4: Receive Messages
+    // channel.consume(
+    //   QUEUE,
+    //   (msg) => {
+    //     console.log(`Message received: ${msg.content.toString()}`);
+    //   },
+    //   {
+    //     noAck: true,
+    //   }
+    // );
+
+    // const QUEUE = "codingtest";
+    channel.assertQueue(QUEUE);
+    // Step 4: Send message to queue
+    channel.sendToQueue(
+      QUEUE,
+      Buffer.from(JSON.stringify({ user: " chihadededines" }))
+    );
+    // channel.sendToQueue(QUEUE, Buffer.from("hello from its coding time"));
+    console.log(`Message send ${QUEUE}`);
+  });
 });
-// const express = require("express");
-// const app = express();
-// const amqp = require("amqplib/callback_api");
-// app.get("/", function (req, res) {
-//   // Step 1: Create Connection
-//   amqp.connect(process.env.RABBITMQ_URL, (connError, connection) => {
-//     if (connError) {
-//       throw connError;
-//     }
-//     // Step 2: Create Channel
-//     connection.createChannel((channelError, channel) => {
-//       if (channelError) {
-//         throw channelError;
-//       }
-//       // Step 3: Assert Queue
-//       const QUEUE = "tsssssssssssssssssss";
-//       channel.assertQueue(QUEUE);
-//       // Step 4: Receive Messages
-//       channel.consume(
-//         QUEUE,
-//         (msg) => {
-//           console.log(`Message received: ${msg.content.toString()}`);
-//         },
-//         {
-//           noAck: true,
-//         }
-//       );
 
-//       // const QUEUE = "codingtest";
-//       // channel.assertQueue(QUEUE);
-//       // // Step 4: Send message to queue
-//       // channel.sendToQueue(QUEUE, Buffer.from("hello from its coding time"));
-//       // console.log(`Message send ${QUEUE}`);
-//     });
-//   });
-//   res.send({ test: process.env.RABBITMQ_URL });
-// });
+app.get("/", function (req, res) {
+  // Step 1: Create Connection
 
-// app.listen(5000, function () {
-//   console.log("Starting hello-world server...");
-// });
+  res.send({ test: process.env.RABBITMQ_URL });
+});
+
+app.listen(5000, function () {
+  console.log("Starting hello-world server...");
+});
