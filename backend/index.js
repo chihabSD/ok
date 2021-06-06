12311dd3326bb50ec614b8d36f52826b557782ed
chihabSD <chihabahmed2012@hotmail.com>
2021-06-06 14:56:47 +0100

    ss

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,27 @@
-const express = require("express");
-const app = express();
+const amqp = require("amqplib/callback_api");
 
-app.get("/", function (req, res) {
-  res.send({ test: process.env.RABBITMQ_URL });
-});
-
-app.listen(5000, function () {
-  console.log("Starting hello-world server...");
+// Step 1: Create Connection
+amqp.connect(process.env.RABBITMQ_URL, (connError, connection) => {
+  if (connError) {
+    throw connError;
+  }
+  // Step 2: Create Channel
+  connection.createChannel((channelError, channel) => {
+    if (channelError) {
+      throw channelError;
+    }
+    // Step 3: Assert Queue
+    const QUEUE = "codingtest";
+    channel.assertQueue(QUEUE);
+    // Step 4: Receive Messages
+    channel.consume(
+      QUEUE,
+      (msg) => {
+        console.log(`Message received: ${msg.content.toString()}`);
+      },
+      {
+        noAck: true,
+      }
+    );
+  });
 });
