f94d3b704f8f2f98a60f0f128197880e5880459a
chihabSD <chihabahmed2012@hotmail.com>
2021-06-07 16:48:27 +0100

    adding serviecs

--- backend/index.js
+++ backend/index.js
@@ -4,45 +4,44 @@ const amqp = require("amqplib/callback_api");
 
 app.post("/login", (req, res) => {
   const { user } = req.body;
-  createSession(user);
-  res.send(user);
+  res.json({ user });
 });
 
-amqp.connect(process.env.RABBITMQ_URL, (connError, connection) => {
-  if (connError) {
-    throw connError;
-  }
-  // Step 2: Create Channel
-  connection.createChannel((channelError, channel) => {
-    if (channelError) {
-      throw channelError;
-    }
-    // Step 3: Assert Queue
-    const QUEUE = "Justtest";
-    channel.assertQueue(QUEUE);
-    // Step 4: Receive Messages
+// amqp.connect(process.env.RABBITMQ_URL, (connError, connection) => {
+//   if (connError) {
+//     throw connError;
+//   }
+//   // Step 2: Create Channel
+//   connection.createChannel((channelError, channel) => {
+//     if (channelError) {
+//       throw channelError;
+//     }
+//     // Step 3: Assert Queue
+//     const QUEUE = "session";
+//     channel.assertQueue(QUEUE);
+//     // Step 4: Receive Messages
 
-    // const QUEUE = "codingtest";
-    channel.assertQueue(QUEUE);
-    // Step 4: Send message to queue
-    channel.sendToQueue(
-      QUEUE,
-      Buffer.from(JSON.stringify({ user: " chihadededines" }))
-    );
-    // channel.sendToQueue(QUEUE, Buffer.from("hello from its coding time"));
-    console.log(`Message send ${QUEUE}`);
+//     // const QUEUE = "codingtest";
+//     channel.assertQueue(QUEUE);
+//     // Step 4: Send message to queue
+//     channel.sendToQueue(
+//       QUEUE,
+//       Buffer.from(JSON.stringify({ user: " chihadededines" }))
+//     );
+//     // channel.sendToQueue(QUEUE, Buffer.from("hello from its coding time"));
+//     console.log(`Message send ${QUEUE}`);
 
-    channel.consume(
-      QUEUE,
-      (msg) => {
-        console.log(`Message received: ${msg.content.toString()}`);
-      },
-      {
-        noAck: true,
-      }
-    );
-  });
-});
+//     channel.consume(
+//       QUEUE,
+//       (msg) => {
+//         console.log(`Message received: ${msg.content.toString()}`);
+//       },
+//       {
+//         noAck: true,
+//       }
+//     );
+//   });
+// });
 
 app.get("/", function (req, res) {
   // Step 1: Create Connection
