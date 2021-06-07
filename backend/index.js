daf115638325428b699109331d9f571337628968
chihabSD <chihabahmed2012@hotmail.com>
2021-06-07 16:56:22 +0100

    adding ss

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const amqp = require("amqplib/callback_api");
 
-app.post("/login", (req, res) => {
+app.get("/login", (req, res) => {
   res.json({ user: "Chihabeddine ahmed" });
 });
 
